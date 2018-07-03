0ed821864751aa7788fd86d639358619d2c83c2f
renyujuan <363886121@qq.com>
2018-07-04 07:59:48 +0800

    icons-init

---
+++ src/pages/home/component/Icons.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="icons">
+    icons
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'homeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -2,17 +2,20 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import homeHeader from './component/Header.vue'
 import homeSwiper from './component/Swiper.vue'
+import homeIcons from './component/Icons.vue'
 export default {
   name: 'Home',
   components: {
     homeHeader,
-    homeSwiper
+    homeSwiper,
+    homeIcons
   }
 }
 </script>
