a4f0fa7ead99af6173a6ddafe92967e6865cbd63
unknown <renyujuan@corp.pandatv.com>
2018-07-09 16:41:39 +0800

    city-search-logic

--- src/pages/city/component/Search.vue
+++ src/pages/city/component/Search.vue
@@ -1,12 +1,65 @@
 <template>
-  <div class="search">
-    <input type="text" placeholder="输入城市名或拼音" class="search-input">
+  <div>
+    <div class="search">
+      <input type="text" placeholder="输入城市名或拼音" class="search-input" v-model="keyword">
+    </div>
+    <div class="search-content" v-show="keyword" ref="search">
+      <ul>
+        <li v-for="item in list" :key="item.id" class="search-item border-bottom">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">未找到匹配结果</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'citySearch'
+  name: 'citySearch',
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  props: {
+    cities: Object
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      this.keyword = this.keyword.trim()
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let item in this.cities) {
+          const _list = this.cities[item]
+          _list.forEach((value) => {
+            const nameIndex = value.name.indexOf(this.keyword)
+            const spellIndex = value.spell.indexOf(this.keyword)
+            if (nameIndex > -1 || spellIndex > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 10)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -24,5 +77,19 @@ export default {
       line-height: .62rem
       text-align: center
       border-radius: .06rem
+      color: #666
+  .search-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    background: #eee
+    .search-item
+      line-height: .62rem
+      padding-left: .2rem
+      background: #fff
       color: #666
 </style>
