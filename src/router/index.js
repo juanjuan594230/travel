8bc99927b1da03ce0d32260813de8afa739226b3
unknown <renyujuan@corp.pandatv.com>
2018-07-10 17:36:43 +0800

    project-finish

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/home.vue'
-import City from '@/pages/city/City.vue'
-import Detail from '@/pages/detail/Detail.vue'
 
 Vue.use(Router)
 
@@ -13,18 +10,18 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => import('@/pages/home/home.vue')
     },
     // 城市选择页面
     {
       path: '/city',
       name: 'City',
-      component: City
+      component: () => import('@/pages/city/City.vue')
     },
     {
       path: '/detail/:id',
       name: 'Detail',
-      component: Detail
+      component: () => import('@/pages/detail/Detail.vue')
     }
   ],
   scrollBehavior (to, from, savedPosition) {
